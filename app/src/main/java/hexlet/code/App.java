f158381504090a1bc0c8899c30884974f2aa4122
ankoz2000 <ankoz2000@mail.ru>
2022-11-28 22:40:24 +0300

    fix prime

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -38,7 +38,6 @@ public class App {
                         + " is wrong answer ;(. Correct answer was '" + game.getRightAnswer() + "'\n"
                         + "Let's try again, " + Greeting.getUsername() + "!");
                 if (game.endsOnFail()) {
-                    Cli.showEndMessage(Greeting.congratulation());
                     return;
                 }
             }
